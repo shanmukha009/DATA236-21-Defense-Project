b914ce05486f3e5adf203453e370331cd7fd4aee
Shanmukh <shanmukha009@gmail.com>
2024-12-14 03:07:39 -0800

    Initial commit for Heroku deployment

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Banner from "@/components/front-end/Banner";
+import Cart from "@/components/front-end/Cart";
+import Feature from "@/components/front-end/Feature";
+import Footer from "@/components/front-end/Footer";
+import Hero from "@/components/front-end/Hero";
+import Navbar from "@/components/front-end/Navbar";
+import { useState } from "react";
+
+export default function Home() {
+  const [showCart, setShowCart] = useState(false);
+
+  return (
+    <main>
+      <Navbar setShowCart={setShowCart} />
+      {showCart && <Cart setShowCart={setShowCart} />}
+      <Hero />
+      <Feature />
+      <Banner />
+      <Footer />
+    </main>
+  );
+}
